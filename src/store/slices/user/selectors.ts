dbc5f8330f9274d44088f7cbc27b4b6c1ff99982
Leroyalle <nikolajmelonov@yandex.ru>
2024-12-07 02:59:05 +0300

    refactor: remove store folder

--- src/store/slices/user/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from '@/store/store';
-
-export const selectCurrent = (state: RootState) => state.user;
-export const selectUser = (state: RootState) => state.user.current;
-export const selectIsAuthenticated = (state: RootState) => state.user.isAuthenticated;
