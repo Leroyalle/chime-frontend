5d3c3af8a87fd458ec7c9bbf4c6bb2ca758cff25
Leroyalle <nikolajmelonov@yandex.ru>
2025-03-05 04:47:23 +0300

    feat(docker): add Dockerfile for containerized

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = { output: 'standalone' };
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig;
