9b60b2938d45dc6bf36bcc2881265ca4db4bb8c7
Leroyalle <nikolajmelonov@yandex.ru>
2025-03-05 22:16:45 +0300

    feat(logo): wrap logo in a link

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { RoutesEnum } from '@/types';
 
 interface Props {
   className?: string;
@@ -7,9 +9,11 @@ interface Props {
 
 export const Logo: React.FC<Props> = ({ className }) => {
   return (
-    <div className={cn('flex gap-x-1 items-center justify-center', className)}>
+    <Link
+      href={RoutesEnum.HOME}
+      className={cn('flex gap-x-1 items-center justify-center', className)}>
       <img className="w-8 h-8" src="/logo.svg" alt="Chime" />
       <span className="text-2xl font-black">Chime</span>
-    </div>
+    </Link>
   );
 };
