d1a34544e39ce5b28b38d53d18248f9873d9ce16
Leroyalle <nikolajmelonov@yandex.ru>
2025-03-06 08:50:25 +0300

    refactor(write-post-actions): update button components

--- src/components/shared/write-post/write-post-actions.tsx
+++ src/components/shared/write-post/write-post-actions.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
-import { Button } from '@nextui-org/react';
 import { Camera, Hash } from 'lucide-react';
 import { SelectImage } from '../select-image';
+import { Button } from '@/components/ui';
+import { Button as ButtonUI } from '@nextui-org/react';
 
 interface Props {
   isPendingCreate?: boolean;
@@ -23,27 +24,27 @@ export const WritePostActions: React.FC<Props> = ({
     <div className={cn('flex items-center justify-between flex-wrap gap-y-1', className)}>
       <div className="flex items-center flex-wrap gap-x-3 gap-y-1">
         <Button
-          startContent={<Camera size={15} />}
           variant="ghost"
           size="sm"
-          as="label"
+          asChild
           className="border-none"
           disabled={isPendingCreate}>
-          Добавить фото
-          <SelectImage name="postImage" onChange={onChangeFile} multiple />
+          <label className="flex items-center gap-x-2" htmlFor="postImage">
+            <Camera size={15} /> Добавить фото
+            <SelectImage name="postImage" id="postImage" onChange={onChangeFile} multiple />
+          </label>
         </Button>
         <Button
-          onPress={() => setTagsIsOpen(!tagsIsOpen)}
-          startContent={<Hash size={15} />}
+          onClick={() => setTagsIsOpen(!tagsIsOpen)}
           variant="ghost"
           size="sm"
-          className="border-none">
-          Хэштег
+          className="border-none flex items-center gap-x-2">
+          <Hash size={15} /> Хэштег
         </Button>
       </div>
-      <Button variant="solid" type="submit" size="sm" className="p-2" isLoading={isPendingCreate}>
+      <ButtonUI type="submit" size="sm" className="p-2" isLoading={isPendingCreate}>
         Отправить
-      </Button>
+      </ButtonUI>
     </div>
   );
 };
